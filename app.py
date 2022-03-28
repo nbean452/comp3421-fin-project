c20693cc6adf9446b606952f6c3215a01cf03b38
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-03-28 16:15:38 +0800

    base functionality added

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'comp3421-fin-project'
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -26,15 +29,18 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    taskid = db.relationship('Task', backref='user')
 
 
 class Task(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    ownerid = db.Column(db.Integer, nullable=False)
+    ownerid = db.Column(db.Integer, db.ForeignKey('user.id'))
     name = db.Column(db.String(20), nullable=False)
-    # completion = db.Column()
+    completion = db.Column(db.Boolean)
     desc = db.Column(db.String(400), nullable=True)
-    # id, ownerid, name, completion, description
+    creationdate = db.Column(db.DateTime, nullable=False,
+                             default=datetime.now)
+    # id, ownerid, name, completion, description, creation date
 
 
 class RegisterForm(FlaskForm):
@@ -104,8 +110,55 @@ def register():
 @app.route('/main')
 @login_required
 def main():
-    return render_template('main.html')
+
+    user = User.query.filter_by(username=current_user.username).first()
+    task_list = Task.query.filter_by(ownerid=user.id)
+
+    return render_template('main.html', user=user, task_list=task_list)
+
+
+@app.route('/addtask', methods=['POST'])
+def addtask():
+
+    taskname = request.form.get('taskname')
+    taskdesc = request.form.get('taskdesc')
+
+    if taskname == '':
+        return redirect(url_for('main'))
+
+    new_task = Task(user=current_user, name=taskname,
+                    completion=False, desc=taskdesc)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('main'))
+
+    # id = db.Column(db.Integer, primary_key=True)
+    # ownerid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # name = db.Column(db.String(20), nullable=False)
+    # completion = db.Column(db.Boolean)
+    # desc = db.Column(db.String(400), nullable=True)
+
+
+@app.route('/updatetask/<int:task_id>')
+def updatetask(task_id):
+
+    task = Task.query.filter_by(id=task_id).first()
+    task.completion = not task.completion
+    db.session.commit()
+    return redirect(url_for('main'))
+
+
+@app.route('/deletetask/<int:task_id>')
+def deletetask(task_id):
+
+    task = Task.query.filter_by(id=task_id).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
+
+    db.create_all()
+
     app.run(debug=True)
