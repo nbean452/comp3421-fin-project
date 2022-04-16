2054eb620ed99ffe819bf2d1168c392ec561b42a
mariewun <102501892+mariewun@users.noreply.github.com>
2022-04-17 03:05:36 +0800

    add functions of change status and check expiry

--- static/js/todo.js
+++ static/js/todo.js
@@ -12,8 +12,15 @@ function validateForm() {
 }
 
 
-$('table tbody td').click(function() {
-    $(this).parent().toggleClass( 'strikeout' );
+$('table tbody tr').click(function() {
+    $(this).toggleClass( 'strikeout change' );
+    var text = $(this).find('td:eq(2)').text();
+    if(text === "Not Finished") {
+      $(this).find('td:eq(2)').text('Finished');
+    }else{
+      $(this).find('td:eq(2)').text('Not Finished')
+    };
+
     
     
 });
@@ -29,3 +36,41 @@ for (i = 0; i < closebtns.length; i++) {
     (this.parentElement).parentElement.style.display = 'none';
   });
 }
+
+function getDateFromString(str) {
+
+  var dateParts = str.split('-');
+  var date = new Date();
+
+  date.setHours(0, 0, 0, 0);
+  date.setDate(dateParts[0]);
+  date.setMonth(dateParts[1] - 1);
+  date.setFullYear(dateParts[2]);
+
+  return date;
+}
+
+document.querySelectorAll('table td').forEach(function(td) {
+
+  var today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  if( getDateFromString(td.innerText) < today ) {
+
+      td.parentElement.style.color = 'red';
+      
+  }  
+});
+
+$("#calendar").fullCalendar({
+  header:{
+    left:'prev',
+    center:'title',
+    right:'next'
+  },
+  
+});
+
+$('#calendarView').on('shown.bs.modal', function () {
+  $("#calendar").fullCalendar('render');
+});
