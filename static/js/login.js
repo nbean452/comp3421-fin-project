1823dba87db8335a6aa1d4458af3838e9eea6202
mariewun <102501892+mariewun@users.noreply.github.com>
2022-04-20 04:24:39 +0800

    Add validation form style change

--- static/js/login.js
+++ static/js/login.js
@@ -6,3 +6,16 @@ function showPassword() {
     x.type = "password";
   }
 }
+
+function validateForm() {
+  var y, x;
+  y = document.getElementById("username");
+  x = document.getElementById("password");
+
+  if (y.value == "") {
+    y.className += " invalid";
+  }
+  if (x.value == "") {
+    x.className += " invalid";
+  }
+}
